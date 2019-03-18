996fd7488dd3611ad7c8a6d07948379918e88886
Melita Clark <beyonce@thinkful.comgit config --global user.email lanis2boys@gmail.com>
2019-03-18 15:43:19 -0700

    updated js/css/html

--- script.js
+++ script.js
@@ -1,3 +1,127 @@
-function getFoodUrl(params){
 
+const BASE_URL = 'https://api.edamam.com/search?app_id=6b4121ea&app_key=6abe9c73f0d4fae10461f81eb26ce3c0'
+
+
+
+function formatQueryParams(params){
+  const myQuery = Object.keys(params)
+  .filter(key=>{
+    return params[key] !==''
+  })
+  .map(key=> `${key}=${encodeURIComponent(params[key])}`)
+  console.log(' params', myQuery)
+  return myQuery.join('&');
 }
+
+function getRecipesURL(diet, mealType){
+  const params = {
+    'q': `${diet}  ${mealType || ''}`,
+    //'part':'snippet', 
+    //'maxResults':25,
+    // 'app_id': '6b4121ea',
+    // 'app_key': '6abe9c73f0d4fae10461f81eb26ce3c0',
+    
+    //'thumbnails': 'url',
+    
+  };
+
+const queryString = formatQueryParams(params)
+const url = BASE_URL + '&' + queryString;
+console.log('URL:', url);
+
+const myResults=$('#search-output') 
+myResults.empty()
+// $('#js-error-message').hide()
+$('#js-error-message').text('Loading, please wait...')
+   fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+     
+      throw new Error(response.statusText);
+    })
+    .then(responseJson =>{ 
+       console.log('JSON RESPONSE:', responseJson) 
+        if(responseJson.hits.length === 0){
+       
+        return $('#js-error-message').show().text(`Search Yields No Results`)
+      }
+        $('#js-error-message').hide()
+        displayResults(responseJson.hits)
+    })
+    
+    .catch(err => {
+      console.error(err)  
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+
+    });
+}
+
+function displayResults(dataArr){
+  const myResults = $('#search-output')
+  myResults.empty()
+   $('#resultsHeader').show()
+
+  const itemIterator = item=>{
+    const recipeObj = item.recipe;
+    const ingredientsHTML = recipeObj.ingredients.map(ingredientObj=>`<li>${ingredientObj.text}</li>`)
+    /*const showIngredients= $('a').on('click', function() {
+      $(this).toggleClass(function(){
+        return ingredientsHTML
+      }); */
+
+    myResults.append(
+    `<section class="card">
+        <div class="result" style="background-image:url('${recipeObj.image}'); height:200px;"></div>
+
+        <p class="urlInfo"> 
+        <a href="${recipeObj.shareAs.replace('http://','https://')}" data-lity>${recipeObj.label}</a>
+                            
+        </p>
+  <nav> <a class="toggle-ingredients" href="#">Click Here To See Ingredients List</a>
+    <ul class="description ingredients-list">
+        ${ingredientsHTML}
+    </ul>
+  </nav>
+    
+
+    </section>
+    `)
+
+
+  }
+  
+    
+
+   dataArr.forEach(itemIterator)
+         
+  }
+
+  function reloadThePage(){
+        window.location.reload();
+    } 
+ 
+
+
+function watchForm() {
+
+  $('body').on('click', '.toggle-ingredients', event => {
+      event.preventDefault()
+       console.log('clicked', event)
+      $(event.target)
+      .parent().find('.ingredients-list')
+      .toggleClass('ingredients-list-visible')
+     
+  })
+
+  $('form').submit(event => {
+    event.preventDefault();
+    const dietStr = $('#js-search-diet').val();
+    const mealTypeStr = $('#js-search-meal').val();
+    getRecipesURL(dietStr, mealTypeStr);
+  });
+  $('#resultsHeader').hide()
+}
+
+$(watchForm);
